c6253f0354c2907d31e6f34dfe6880e9a12de4f5
vladimirdzotov <zotov@bluewin.ch>
2014-07-26 21:15:06 +0200

    commiting assigment for peer evaluation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
-## Put comments here that give an overall description of what your
-## functions do
+## In this function we inversing inputed matrix bu function solve() and caching it.
+## So, there will be no need to calculate inverse every time. 
 
-## Write a short comment describing this function
+## Getting the matrix inputed, invert it. 
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(x = matrix()) { 
+        s <- NULL                           
+        setMatrix <- function(y) {                                    
+                x <<- y                     
+                s <<- NULL
+        }
+        getMatrix <- function() x
+        setInverse <- function(solve) s <<- solve
+        getInverse <- function() s
+        list(setMatrix = setMatrix, getMatrix = getMatrix,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+
+## Here we getting cache and print it 
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        s <- x$getInverse()
+        if(!is.null(s)) {
+                message("getting cached datta")
+                return(s)
+        }
+        data <- x$getMatrix()  
+        s <- solve(data, ...) 
+        x$setInverse(s)
+        s
 }
